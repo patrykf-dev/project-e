1c64d1c209bdc48f3d3fb95246b7d012c06de620
gsrmeen <109578061+patrykf-dev@users.noreply.github.com>
2022-07-26 11:28:58 +0200

    Add angular velocity limit

--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -8,20 +8,30 @@ public class SpaceshipMovement : MonoBehaviour
     private float _rotationForce = 10f;
     [SerializeField]
     private float _maxVelocity = 2.5f;
+    [SerializeField]
+    private float _maxAngularVelocity = 70f;
 
     private Rigidbody2D _rigidbody;
+    private float _rotationInput;
+    private float _accelerationInput;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        _rotationInput = Input.GetAxis("Horizontal");
+        _accelerationInput = Input.GetAxis("Vertical");
+    }
+
     private void FixedUpdate()
     {
-        var rotation = Input.GetAxis("Horizontal");
-        var acceleration = Input.GetAxis("Vertical");
-        _rigidbody.AddRelativeForce(Vector2.up * (acceleration * _accelerationForce));
-        _rigidbody.AddTorque(-rotation * _rotationForce);
+        _rigidbody.AddRelativeForce(Vector2.up * (_accelerationInput * _accelerationForce));
+        _rigidbody.AddTorque(-_rotationInput * _rotationForce);
+
         _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, _maxVelocity);
+        _rigidbody.angularVelocity = Mathf.Clamp(_rigidbody.angularVelocity, -_maxAngularVelocity, _maxAngularVelocity);
     }
 }
